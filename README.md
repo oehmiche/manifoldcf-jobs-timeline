14e2bb22d370d18b7d96f8057545f8099d9d258a
Odilo Oehmichen <odilo.oehmichen@gmail.com>
2016-12-18 15:59:27 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# manifoldcf-jobs-timeline
-Simple jQuery plugin providing an overview of all scheduled manifoldcf jobs.
+# ManifoldCF Jobs Timeline 
+
+This jQuery Plugin provides an easy way to create an overview of all scheduled jobs of a
+[ManifoldCF](https://manifoldcf.apache.org/en_US/index.html) using the already existing [simpleTime jQuery Plugin](https://github.com/eScienceCenter/simpleTimeline) from [eScience-Center](https://github.com/eScienceCenter).
+
+The ManifoldCF UI does not provide an aggregated view of all jobs configured. - For this you have to select each job an check the scheduling tab: <img src="screenshots/screenshot-manifoldcf-ui.png" alt="ManifoldCF UI" width="600">
+
+The ManifoldCF Jobs Timeline fills this gap using the [servlet-based JSON API](https://manifoldcf.apache.org/release/release-2.5/en_US/programmatic-operation.html), which is available under ```/mcf-api-service/json/``` on default installation, and making it possible to integrate the job overview whereever you want:
+<img src="screenshots/screenshot-manifoldcf-timeline.png" alt="manifoldcf timeline" width="600">
+
+Currently only times and no weekday or dates are considered for the timeline creation.
+
+## Usage
+
+### Requirements
+* [jQuery](https://jquery.com/) (tested with Version 1.12.4)
+* [simpleTimeline.js, simpleTimeline.css](https://github.com/eScienceCenter/simpleTimeline)
+* resize.js
+* this plugin (manifoldcfJobsTimeline.js)
+
+**Important Note**
+You need to adjust the CORS headers of your ManifoldCF json api (provided by the mcf-api-service webapp) when using this plugin on a different domain.
+
+### Example
+```html
+<head>
+	<link rel="stylesheet" href="css/simpleTimeline.css" />
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+	<script src="js/resize.js"></script>
+	<script src="js/simpleTimeline.js"></script>
+	<script src="js/manifoldcfJobsTimeline.js"></script>
+	...
+</head>
+<body>		
+	<div id="timeline"></div>
+	
+	<script>
+		$(document).ready(function() {
+			var options = {
+				baseUrl: "http://localhost:8345"
+			};
+			$('#timeline').manifoldCfJobsTimeline(options);
+		});
+	</script>
+</body>
+```
+Set the ```baseUrl``` pointing to your ManifoldCF server.
+
+See (example.html) for a working example using static json-data.
+
+
+
+## Features
+* generates an overview of all jobs
+* consider 
+* jobs without duration get a default length and will be marked dotted
+* provides job detail on click
+* direct linking to the jobs detail page
+
+## Open Issues
+* no support for weekday/date specific job schedules
+* different colouring for each job
+* more actions in the detail view
+
+
+## License
+ManifoldCF Jobs Timeline Plugin is released under the MIT license. See (LICENSE) for details.
---
+++ js/manifoldcfJobsTimeline.js
@@ -0,0 +1,133 @@
+/*
+	jQuery manifoldCfJobsTimeline plugin v0.1
+	
+	Licensed under the MIT License
+	https://opensource.org/licenses/MIT
+*/
+(function ( $ ) {
+ 
+    $.fn.manifoldCfJobsTimeline = function( options ) {
+
+        var self = $(this);
+
+	    var defaults = {
+			jobsEndpoint : options.baseUrl + "/mcf-api-service/json/jobs",
+			viewJobUrl : options.baseUrl + "/mcf-crawler-ui/viewjob.jsp?jobid=",
+			// simpleTimeline options
+			toggle : '',
+			barHeight: 25,
+			headerHeight: 25,
+			fontSize: 16,
+			verticalBarSpacing: 5,
+			phases: [
+				{ start: 0, end: 24, indicatorsEvery: 1, share: 1 }
+			],
+			formatHeader: function (v) {
+				return v;
+			}
+	    };
+
+	    var settings = $.extend( {}, defaults, options);
+
+		var baseUrl = settings.baseUrl;
+		var jobsEndpoint = settings.jobsEndpoint;
+		var viewJobUrl = settings.viewJobUrl;
+
+		$.getJSON(jobsEndpoint, function(jobs) {
+			var data = [];
+			$.each(jobs.job, function(index, job){
+				var oneJob = [];
+				var schedules = [];
+				// if there more than one schedule configured, we get an array
+				if(job.schedule instanceof Array) {
+					schedules = job.schedule;
+				} else {
+					schedules.push(job.schedule);
+				}
+				$.each(schedules, function(index, schedule){
+					// there is not always a schedule given
+					if(schedule) {
+						var start = starttime(schedule);
+						var end = endtime(start, schedule);
+						// use different style for jobs schedules without duration
+						var noDuration = (schedule.duration) ? false: true;
+
+						// split a job schedule if it crosses midnight
+						if(end <= 24) {
+							oneJob.push(createJobEntry(index, job, schedule, start, end, noDuration));
+						} else {
+							oneJob.push(createJobEntry(index, job, schedule, start, 24, noDuration));
+							oneJob.push(createJobEntry(index, job, schedule, 0, end - 24, noDuration));
+						}
+					}
+					oneJob.sort(sortByStartTime);
+				})
+				data.push(oneJob);
+			});
+			data.sort(sortArrayByFirstStartTime);
+			self.simpleTimeline(settings, data);
+		});
+
+
+		// ** private functions **
+		function createJobEntry(index, job, schedule, start, end, noDuration) {
+			var css = { "color": "darkblue", "background-color": "lightblue"};
+			if(noDuration) 
+				css = { "color": "darkblue", "background-color": "lightblue", "border-style": "dotted"}
+			return {
+				id: job.id + index, 
+				label:job.description, 
+				start: start, 
+				end: end, 
+				css: css, 
+				popup_html: info_html(job, schedule)
+			}
+		};
+		
+		function sortByStartTime(a, b) {
+			return ((a.starttime < b.starttime) ? -1 : 1);
+		};
+		
+		function sortArrayByFirstStartTime(jobArray1, jobArray2) {
+			return sortByStartTime(jobArray1[0], jobArray2[0]);
+		};
+		
+		function info_html(job, schedule) {
+			var html = "<b>" + job.description +"</b><br>"
+			+ "Startmode: " + job.start_mode +"<br>"
+			+ "Starttime: " + printTime(schedule.hourofday, schedule.minutesofhour) + "<br>" 
+			+ "Duration: " + printDurationInMinutes(schedule.duration) + " <br>"
+			+ "<a href='" + viewJobUrl+job.id+"' target='_blank'>View Job</a>";
+			return html;
+		};
+		
+		function printTime(hour, minutes) {
+			return hour.value + ":" + (minutes.value.length == 1 ? '0' : '') + minutes.value;
+		};
+		
+		function printDurationInMinutes(duration) {
+			return (duration ? (parseInt(duration) / (1000 * 60)) + " min " : "-");
+		};
+		
+		function starttime(schedule) {
+			return parseInt(schedule.hourofday.value) + (parseInt(schedule.minutesofhour.value) / 60);
+		};
+		
+		function endtime(starttime, schedule) {
+			if(schedule.duration) {
+				return starttime + (parseInt(schedule.duration) / (1000 * 60 * 60));
+			} 
+			return starttime + 1;
+		};
+		
+		function bind_popup() {
+			var sel_item_id = $('.selected').data('id');
+			if(typeof sel_item_id == 'undefined') {
+				alert('Ain\'t nothin\' selected, yo!');
+				return;
+			}
+		}; 
+    };
+ 
+}( jQuery ));
+
